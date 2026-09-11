3967b2e2c79a342f2b3512b906afc392b986502a
mauricio henriques <111745353+mauricio-henriques@users.noreply.github.com>
2026-09-11 10:30:58 -0300

    Update Registro.java

--- Questão1/Registro.java
+++ Questão1/Registro.java
@@ -1,1 +1,11 @@
-
+class Registro {
+    private String nome;
+    private String idade;
+    private String lugar;
+    
+    public Registro(String nome, String idade, String lugar) {
+        this.nome = nome;
+        this.idade = idade;
+        this.lugar = lugar;
+    }
+}
