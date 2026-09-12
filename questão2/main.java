b826909be44ccb13c190af9f9d232470b8b8e4f0
mauricio henriques <111745353+mauricio-henriques@users.noreply.github.com>
2026-09-11 23:47:15 -0300

    Create main.java

---
+++ questão2/main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int quantidade = s.nextInt();
+    for(int i = 0; i < quantidade; i++){
+      	System.out.print("Nota do estudante: ");
+        ponto = s.nextFloat();
+    }
+		
+		System.out.println("Ola "+nome+"\nVoce tem "+idade+" ano(s) e mora em "+lugar);
+	}
+}
